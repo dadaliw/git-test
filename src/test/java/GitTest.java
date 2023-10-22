e6430277398f3be53a8dc4d59dd9c9eaad33bdda
123945020 <123945020@qq.com>
2023-10-22 11:02:06 +0800

    git three

--- src/test/java/GitTest.java
+++ src/test/java/GitTest.java
@@ -3,5 +3,7 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
     }
 }
